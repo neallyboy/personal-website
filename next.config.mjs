8c73c6bb85f9b68377c617fc5b472447c68082bb
Neal Miran <neal.miran@gmail.com>
2026-05-14 12:34:33 -0400

    fix(csp): add consentcdn.cookiebot.com to frame-src and style-src

--- next.config.mjs
+++ next.config.mjs
@@ -11,11 +11,11 @@ const securityHeaders = [
     value: [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://nealmiran.com https://www.googletagmanager.com https://www.google-analytics.com https://*.clarity.ms https://consent.cookiebot.com https://consentcdn.cookiebot.com",
-      "style-src 'self' 'unsafe-inline' https://consent.cookiebot.com https://consentcdn.cookiebot.com",
+      "style-src 'self' 'unsafe-inline' https://consent.cookiebot.com https://consentcdn.cookiebot.com https://fonts.googleapis.com",
       "img-src 'self' data: blob: https://nealmiran.com https://www.googletagmanager.com https://www.google-analytics.com https://*.clarity.ms https://c.bing.com https://consent.cookiebot.com https://consentcdn.cookiebot.com https://fonts.gstatic.com",
       "font-src 'self' data: https://fonts.gstatic.com",
       "connect-src 'self' https://nealmiran.com https://www.googletagmanager.com https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://region1.google-analytics.com https://*.clarity.ms https://dc.services.visualstudio.com https://consent.cookiebot.com https://consentcdn.cookiebot.com",
-      "frame-src https://nealmiran.com https://www.googletagmanager.com https://consent.cookiebot.com",
+      "frame-src https://nealmiran.com https://www.googletagmanager.com https://consent.cookiebot.com https://consentcdn.cookiebot.com",
       "object-src 'none'",
       "base-uri 'self'",
     ].join("; "),
