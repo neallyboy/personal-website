245b8d70dc298816bc288c4db525d3203ab3e97f
Neal Miran <neal.miran@gmail.com>
2026-05-14 07:32:54 -0400

    fix(analytics): revert to namespace import for vanilla-cookieconsent
    Module has no default export; default import breaks the Turbopack build.

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import CookieConsent from "vanilla-cookieconsent";
+import * as CookieConsent from "vanilla-cookieconsent";
 import "vanilla-cookieconsent/dist/cookieconsent.css";
 
 function grantAnalytics() {
